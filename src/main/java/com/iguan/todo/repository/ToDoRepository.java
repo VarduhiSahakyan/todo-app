7141e7a6b5f8525361d24777a37a21fa99e9853d
varduhisa <vardsahakyan33@gmail.com>
2023-05-09 11:30:55 +0400

    create repository

---
+++ src/main/java/com/iguan/todo/repository/ToDoRepository.java
@@ -0,0 +1,19 @@
+package com.iguan.todo.repository;
+
+import com.iguan.todo.domain.ToDo;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+
+@Repository
+public interface ToDoRepository extends CrudRepository<ToDo, Integer> {
+
+    List<ToDo> findAll(Pageable pageable);
+
+    ToDo findToDoById(Integer id);
+
+}
