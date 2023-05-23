6a3c545059185b7bc8e8ebaaeae7a2f92fdbd0b8
varduhisa <vardsahakyan33@gmail.com>
2023-05-23 14:45:25 +0400

    add configs

--- src/main/java/com/iguan/todo/ToDoApplication.java
+++ src/main/java/com/iguan/todo/ToDoApplication.java
@@ -1,20 +1,14 @@
 package com.iguan.todo;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
 public class ToDoApplication {
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
     public static void main(String... args) {
         SpringApplication.run(ToDoApplication.class, args);
---
+++ src/main/java/com/iguan/todo/conf/Config.java
@@ -0,0 +1,13 @@
+package com.iguan.todo.conf;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
